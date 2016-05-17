9cf4e972b14cae354c49cee9f7cc975263123851
Philipp Welsch <pwelsch86@gmail.com>
2016-05-18 00:04:01 +0200

    Finally had time to fix up main

--- src/RayTracer.cpp
+++ src/RayTracer.cpp
@@ -14,19 +14,14 @@ vec3 color(const ray& r)
 int main()
 {	
 	PNG img(200, 100, 3);
-	int x = 0; int y = 0;
-
+	
 	for (int j = img.y - 1; j >= 0; j--)
 	{
 		for (int i = 0; i < img.x ; i++)
 		{
 			vec3 col(float(i) / float(img.x), float(j) / float(img.y), 0.2f);
-			img.setPixel(x, y, 255.99f * col.x, 255.99f * col.y, 255.99f * col.z);
-
-			x++;
+			img.setPixel(i, img.y - 1 - j, 255.99f * col.x, 255.99f * col.y, 255.99f * col.z);
 		}
-		y++;
-		x = 0;
 	}
 
 	if (!img.saveToImage("test.png")) std::cout << "Failed to save image" << std::endl;
